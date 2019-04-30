06d7ed49aaeca3998727633430da1c4d690f7138
LandingHeart <land.liu0123@gmail.com>
2019-04-30 16:20:47 -0400

    updates

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import os
-path = "/Users/shinan/Desktop/TestingFolder"
+path = "/Users/shinan/Desktop/pythonTu"
 arr = os.listdir(path)
 
 for file in arr:
@@ -7,3 +7,22 @@ for file in arr:
 
 newlist = [newlist[0] for newlist in arr]
 print newlist
+
+def removeDub(dubChar):
+	final_list = []
+	for char in dubChar:
+
+		if char not in final_list:
+			arr.append("dub")
+			final_list.append(char)
+	return final_list
+
+def appendDub(dub):
+	for dubC in dub:
+		if dubC not in dub:
+			arr.append("dub")
+	return 
+dubStr = removeDub(newlist)
+
+
+
